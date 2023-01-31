dec8506689244a1dc74b487ab62f20f1634f4942
PossiblyAxolotl <judah@shaw.ca>
2023-01-31 15:05:58 -0600

    Add files via upload

---
+++ Example/main.lua
@@ -0,0 +1,47 @@
+local gfx <const> = playdate.graphics
+
+import "coreLibs/graphics"
+import "coreLibs/object"
+import "pdParticles"
+
+local particleA = ParticleCircle(200, 120)
+
+particleA:setSize(5,7)
+
+particleA:setSpeed(1, 5)
+
+particleA:setMode(Particles.modes.LOOP)
+
+particleA:setThickness(0, 3)
+
+particleA:setBounds(0,0,400,240)
+
+local particleB = ParticleCircle(200, 120)
+
+particleB:setSize(15,25)
+
+particleB:setSpeed(1, 3)
+
+particleB:setMode(Particles.modes.LOOP)
+
+particleB:setBounds(0,0,400,240)
+
+particleB:setClr(gfx.kColorXOR)
+
+
+function playdate.update()
+    gfx.clear()
+    Particles:update()
+
+    if playdate.buttonJustPressed(playdate.kButtonA) then
+        particleA:add(20)
+    end
+
+    if playdate.buttonJustPressed(playdate.kButtonB) then
+        particleB:add(5)
+    end
+
+    if playdate.buttonJustPressed(playdate.kButtonUp) then
+        Particles:clearAll()
+    end
+end
---
+++ Example/pdParticles.lua
@@ -0,0 +1,260 @@
+local circles = {}
+
+-- base particle class
+class("Particle").extends()
+function Particle:init(x, y)
+    self.x = x or 0
+    self.y = y or 0
+    self.size = {10,10}
+    self.spread = {0,359}
+    self.speed = {1,1}
+    self.thickness = {0,0}
+    self.lifespan = {1,1}
+    self.decay = 1
+    self.particles = {}
+    self.colour = playdate.graphics.kColorBlack
+    self.bounds = {0,0,0,0}
+    self.mode = 0
+
+    if self.type == 1 then
+        circles[#circles+1] = self
+    end
+end
+
+-- [[ SETTERS AND GETTERS ]] --
+
+-- movement
+function Particle:moveTo(x,y)
+    self.x = x
+    self.y = y
+end
+
+function Particle:moveBy(x,y)
+    self.x += x
+    self.y += y
+end
+
+function Particle:getPos()
+    return self.x, self.y
+end
+
+-- size
+function Particle:setSize(min, max)
+    self.size = {min, max or min}
+end
+
+function Particle:getSize()
+    return self.size[1], self.size[2]
+end
+
+-- mode
+function Particle:setMode(mode)
+    self.mode = mode
+end
+
+function Particle:getMode()
+    return self.mode
+end
+
+-- spread
+function Particle:setSpread(min,max)
+    self.spread = {min, max or min}
+end
+
+function Particle:getSpread()
+    return self.spread[1], self.spread[2]
+end
+
+-- speed
+function Particle:setSpeed(min,max)
+    self.speed = {min, max or min}
+end
+
+function Particle:getSpeed()
+    return self.speed[1], self.speed[2]
+end
+
+-- thickness
+function Particle:setThickness(min, max)
+    self.thickness = {min, max or min}
+end
+
+function Particle:getThickness()
+    return self.thickness[1], self.thickness[2]
+end
+
+-- lifespan
+function Particle:setLifespan(min,max)
+    self.lifespan = {min, max or min}
+end
+
+function Particle:getLifespan()
+    return self.lifespan[1], self.lifespan[2]
+end
+
+-- colour
+function Particle:setClr(colour)
+    self.colour = colour
+end
+
+function Particle:getClr()
+    return self.colour
+end
+
+-- bounds
+function Particle:setBounds(x1, y1, x2, y2)
+    self.bounds = {x1,y1,x2,y2}
+end
+
+function Particle:getBounds()
+    return self.bounds[1],self.bounds[2],self.bounds[3],self.bounds[4]
+end
+
+-- decay
+function Particle:setDecay(decay)
+    self.decay = decay
+end
+
+function Particle:getDecay()
+    return self.decay
+end
+
+-- particles
+function Particle:getParticles()
+    return self.particles
+end
+
+function Particle:clearParticles()
+    self.particles = {}
+end
+
+function Particle:update()
+end
+
+-- [[ PARTICLE MODES ]] --
+
+-- circles
+local function decayCirc(circs, decay)
+    for part = 1, #circs, 1 do
+        local circ = circs[part]
+        circ.size -= decay
+
+        if circ.size <= 0 then
+            circ.size = 0
+        end
+
+        circs[part] = circ
+    end
+
+    for part = 1, #circs, 1 do
+        local circ = circs[part]
+        if circ.size <= 0 then
+            table.remove(circs,part)
+            break
+        end
+    end
+
+    return circs
+end
+
+local function disappearCirc(circs)
+    for part = 1, #circs, 1 do
+        local circ = circs[part]
+        circ.lifespan -= .1
+    end
+    for part = 1, #circs, 1 do
+        local circ = circs[part]
+        if circ.lifespan <= 0 then
+            table.remove(circs,part)
+            break
+        end
+    end
+
+    return circs
+end
+
+local function loopCirc(circs, bounds)
+    if bounds[3] > bounds[1] and bounds[4] > bounds[2] then
+        local xDif , yDif = bounds[3] - bounds[1], bounds[4] - bounds[2]
+        for part = 1, #circs, 1 do
+            local circ = circs[part]
+            if circ.x > bounds[3] then circ.x -= xDif
+            elseif circ.x < bounds[1] then circ.x += xDif end
+            if circ.y > bounds[4] then circ.y -= yDif
+            elseif circ.y < bounds[2] then circ.y += yDif end
+        end
+    end
+
+    return circs
+end
+
+class("ParticleCircle", {type = 1}).extends(Particle)
+
+function ParticleCircle:create(amount)
+    for i = 1, amount, 1 do
+        local part = {
+            x = self.x,
+            y = self.y,
+            dir = math.random(self.spread[1],self.spread[2]),
+            size = math.random(self.size[1],self.size[2]),
+            speed = math.random(self.speed[1],self.speed[2]),
+            lifespan = math.random(self.lifespan[1],self.lifespan[2]),
+            thickness = math.random(self.thickness[1],self.thickness[2]),
+            decay = self.decay
+        }
+
+        self.particles[#self.particles+1] = part
+    end
+end
+
+function ParticleCircle:add(amount)
+    self:create(amount)
+end
+
+function ParticleCircle:update()
+    local w = playdate.graphics.getLineWidth()
+    local c = playdate.graphics.getColor()
+    playdate.graphics.setColor(self.colour)
+    for part = 1, #self.particles, 1 do
+        local circ = self.particles[part]
+        if circ.thickness < 1 then
+            playdate.graphics.fillCircleAtPoint(circ.x,circ.y,circ.size)
+        else
+            playdate.graphics.setLineWidth(circ.thickness)
+            playdate.graphics.drawCircleAtPoint(circ.x, circ.y, circ.size)
+        end
+
+        circ.x += math.sin(math.rad(circ.dir)) * circ.speed
+        circ.y -= math.cos(math.rad(circ.dir)) * circ.speed
+
+        self.particles[part] = circ
+    end
+    playdate.graphics.setLineWidth(w)
+    playdate.graphics.setColor(c)
+    if self.mode == 1 then
+        local newCircs = decayCirc(self.particles, self.decay)
+        self.circs = newCircs
+    elseif self.mode == 0 then
+        local newCircs = disappearCirc(self.particles)
+        self.circs = newCircs
+    elseif self.mode == 2 then
+        local newCircs = loopCirc(self.particles, self.bounds)
+        self.circs = newCircs
+    end
+end
+
+class("Particles").extends()
+
+Particles.modes = {DISAPPEAR = 0, DECAY = 1, LOOP = 2, STAY = 3}
+
+function Particles:update()
+    for circle = 1, #circles, 1 do
+        circles[circle]:update()
+    end
+end
+
+function Particles:clearAll()
+    for circle = 1, #circles, 1 do
+        circles[circle]:clearParticles()
+    end
+end
